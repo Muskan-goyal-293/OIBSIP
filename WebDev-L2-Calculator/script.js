475f50f2e8e425c5fe850494c43548636ec1444b
muskan <m76736238@gmail.com>
2026-08-19 15:07:05 +0530

    add all ui and cdd , and nav bar theme animation  or color feature

---
+++ WebDev-L2-Calculator/script.js
@@ -0,0 +1,63 @@
+//  navbar animation function
+function animateNavText() {
+  const navDiv = document.querySelector(".navDiv");
+  const navH1 = document.querySelector(".navDiv h1");
+  const navH1Text = navH1.innerText.split("");
+  
+  let sum = "";
+  navH1Text.forEach((element) => {
+    sum += `<span>${element}</span>`;
+  });
+
+  navDiv.innerHTML = sum;
+
+  const navSpan = document.querySelectorAll(".navDiv span");
+
+  let count = 0;
+  let interval = null;
+
+  navSpan.forEach((val) => {
+    clearInterval(interval);
+    interval = setInterval((val) => {
+      navSpan[count].style.opacity = 1;
+      count++;
+      if (count > navSpan.length - 1) {
+        clearInterval(interval);
+      }
+    }, 250);
+  });
+}
+animateNavText();
+
+
+// theme related code
+
+const root = document.documentElement;
+const themeButton = document.querySelector(".themeButton button");
+let theme = localStorage.getItem("themeValue") || "dark" ;
+
+function themeSelect() {
+    if (theme == "dark") {
+    root.style.setProperty("--first", "black");
+    root.style.setProperty("--sec", "#101010");
+    root.style.setProperty("--third", "#3A3A3A");
+    root.style.setProperty("--four", "white");
+    localStorage.setItem("themeValue", "dark");
+    themeButton.innerText = "Dark 🌑"
+    theme = "light"
+  } else {
+    root.style.setProperty("--first", "white");
+    root.style.setProperty("--sec", "#EEEEEE");
+    root.style.setProperty("--third", "#DADADA");
+    root.style.setProperty("--four", "black");
+    localStorage.setItem("themeValue", "light");
+    themeButton.innerText ="Light 🌕"
+theme ="dark"
+}
+}
+themeSelect()
+
+themeButton.addEventListener("click",()=>{
+    themeSelect();
+})
+
